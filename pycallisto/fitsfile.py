2bb276d10b66882b879e4c2f7e177e221fb6aee2
andrekorol <anrobits@yahoo.com.br>
2019-08-17 16:55:15 -0300

    Reorganize project structure

--- pycallisto/fitsfile.py
+++ pycallisto/fitsfile.py
@@ -1,25 +1,25 @@
-# Pycallisto: Python tools for analyzing data from the e-Callisto International
-# Network of Solar Radio Spectrometers
+# FitsFile: Classes and methods for reading and plotting data from FITS files
 # Copyright (C) 2019 Andre Rossi Korol
 #
-# This file is part of Pycallisto.
+# This file is part of PyCallisto.
+# PyCallisto: Python tools for analyzing data from the e-Callisto International
+# Network of Solar Radio Spectrometers
 #
-# Pycallisto is free software: you can redistribute it and/or modify
+# PyCallisto is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# Pycallisto is distributed in the hope that it will be useful,
+# PyCallisto is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with Pycallisto. If not, see <https://www.gnu.org/licenses/>.
+# along with PyCallisto. If not, see <https://www.gnu.org/licenses/>.
 
 import os
 import fnmatch
-import math
 import json
 import itertools
 from datetime import timedelta
@@ -412,43 +412,3 @@ class ECallistoFitsFile(FitsFile):
         if show:
             plt.show()
         # TODO: Fix the plot_fits_linear_regression method
-
-
-class ChromosphericEvaporationFitsFile(ECallistoFitsFile):
-    def __init__(self, filename: str = None):
-        ECallistoFitsFile.__init__(self, filename)
-        self.front = {}
-
-    def set_front_velocity(self, inf_front_time,
-                           sup_front_time,
-                           velocity=None):
-        front = self.front
-        if velocity is not None:
-            front['velocity'] = velocity
-        else:
-            lin_reg_fn = self.get_fits_linear_regression_function()
-            inf_front_freq = lin_reg_fn(inf_front_time)
-            sup_front_freq = lin_reg_fn(sup_front_time)
-            front['freq_diff'] = abs(sup_front_freq - inf_front_freq)
-
-            inf_density = (inf_front_freq / 8.98e-03) ** 2
-            sup_density = (sup_front_freq / 8.98e-03) ** 2
-            front['density_diff'] = abs(sup_density - inf_density)
-
-            Nq = 4.6e+8
-            H = 7e+4
-            inf_height = math.log(Nq / inf_density) * H
-            sup_height = math.log(Nq / sup_density) * H
-
-            front['time_diff'] = (sup_front_time - inf_front_time) * 3600
-            front['height_diff'] = sup_height - inf_height
-            front['df_over_dt'] = front['freq_diff'] / front['time_diff']
-
-            self.front['velocity'] = round(front['height_diff'] /
-                                           front['time_diff'], 1)
-
-    def get_front_velocity(self):
-        return self.front['velocity']
-
-    def get_front(self):
-        return self.front
