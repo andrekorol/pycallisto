5701fe6b03315c8cd3110bc93abf8be74605d9b1
andrekorol <anrobits@yahoo.com.br>
2020-05-16 01:11:39 -0300

    Remove GPLv3 preamble

--- pycallisto/fitsfile.py
+++ pycallisto/fitsfile.py
@@ -1,23 +1,3 @@
-# FitsFile: Classes and methods for reading and plotting data from FITS files
-# Copyright (C) 2019 Andre Rossi Korol
-#
-# This file is part of PyCallisto.
-# PyCallisto: Python tools for analyzing data from the e-Callisto International
-# Network of Solar Radio Spectrometers
-#
-# PyCallisto is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# PyCallisto is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with PyCallisto. If not, see <https://www.gnu.org/licenses/>.
-
 import fnmatch
 import itertools
 import json
