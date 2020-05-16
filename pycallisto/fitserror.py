5fd9c36f07b3a8ccc78d13fdd05d47a2f6e6d7f2
andrekorol <anrobits@yahoo.com.br>
2020-05-16 03:25:21 -0300

    Go back to a GPLv3 license

--- pycallisto/fitserror.py
+++ pycallisto/fitserror.py
@@ -1,3 +1,24 @@
+# FitsError: Custom exceptions for FITS related errors
+# Copyright (C) 2020 Andre Rossi Korol
+#
+# This file is part of PyCallisto.
+# PyCallisto: Python tools for analyzing data from the e-Callisto International
+# Network of Solar Radio Spectrometers
+#
+# PyCallisto is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyCallisto is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyCallisto. If not, see <https://www.gnu.org/licenses/>.
+
+
 class FitsFileError(Exception):
     """Exception to be raised when astropy.io.fits.open throws an OSError"""
 
