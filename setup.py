a5583087298a50dd3228be3c25899799ba22946f
andrekorol <anrobits@yahoo.com.br>
2020-05-16 02:43:38 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,15 @@ try:
 except ImportError:
     from distutils.core import setup
 
-config = {
-    "name": "pycallisto",
-    "description": "Python library for analyzing data from the e-Callisto "
+setup(
+    name="pycallisto",
+    description="Python library for analyzing data from the e-Callisto "
     "International Network of Solar Radio Spectrometers",
-    "author": "Andre Rossi Korol",
-    "url": "https://github.com/andrekorol/pycallisto",
-    "download_url": "https://github.com/andrekorol/pycallisto/archive/" "master.zip",
-    "author_email": "anrobits@yahoo.com.br",
-    "version": "0.1.0",
-    "install_requires": ["numpy", "astropy", "matplotlib"],
-    "packages": ["pycallisto"],
-}
-
-setup(**config)
+    author="Andre Rossi Korol",
+    url="https=//github.com/andrekorol/pycallisto",
+    download_url="https=//github.com/andrekorol/pycallisto/archive/" "master.zip",
+    author_email="anrobits@yahoo.com.br",
+    version="0.1.0",
+    install_requires=["numpy", "astropy", "matplotlib"],
+    packages=["pycallisto"],
+)
