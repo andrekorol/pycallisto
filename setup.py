37f8d28e9b49d0cc5b4f0c18b52e7dbcccb30e64
andrekorol <anrobits@yahoo.com.br>
2020-05-22 17:56:38 -0300

    Bump version to 0.2.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     url="https=//github.com/andrekorol/pycallisto",
     download_url="https=//github.com/andrekorol/pycallisto/archive/master.zip",
     author_email="anrobits@yahoo.com.br",
-    version="0.1.0",
+    version="0.2.0",
     install_requires=["numpy", "astropy", "matplotlib"],
     packages=["pycallisto"],
     package_data={"pycallisto": ["languages.json"]},
